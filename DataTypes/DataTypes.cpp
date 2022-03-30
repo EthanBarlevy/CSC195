858da2f018782a7f3757c98da04f429fb2b84d34
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2022-03-30 16:20:11 -0600

    Changed the name of of the solution and finised the assignment Primitive Data Types

--- DataTypes/DataTypes.cpp
+++ DataTypes/DataTypes.cpp
@@ -1,5 +1,4 @@
-// DataTypes.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
 #include <string>
@@ -8,36 +7,53 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
-
-
-    int i = 0;
-    float f = 23.5;
-    bool b = 0;
     string name;
-
-
-    const float TAX = 0.055;
     char initial;
+    unsigned int age;
+    bool isAdult;
+    unsigned int zipcode;
+    float wage;
+    unsigned int daysWorked;
+    unsigned int hoursWorkedPerDay[7];
+    const float TAX = 0.08;
 
-    int ages[4];
-
+    cout << "Enter first name: ";
     cin >> name;
-    cout << name << endl;
+
+    cout << "Enter the first initial of last name: ";
+    cin >> initial;
+
+    cout << "Enter age: ";
+    cin >> age;
+
+    if (age >= 18) {
+        isAdult = true;
+    }
+
+    cout << "Enter zipcode: ";
+    cin >> zipcode;
+
+    cout << "Enter wage: ";
+    cin >> wage;
+
+    cout << "Enter how many days worked last week: ";
+    cin >> daysWorked;
+
+    unsigned int totalHours = 0;
+    for (int i = 0; i < daysWorked; i++) {
+        cout << "Enter hours worked on day " << i + 1 << ": ";
+        cin >> hoursWorkedPerDay[i];
+        totalHours += hoursWorkedPerDay[i];
+    }
+
+    float grossIncome = totalHours * wage;
+    float netIncome = grossIncome - (TAX * grossIncome);
+
+    cout << name << " worked for " << totalHours << " hours at $" << wage << " per hour.\n";
+    cout << "Gross Income: $" << grossIncome << endl;
+    cout << "Net Income: $" << netIncome << endl;
 
 
-    if (i) cout << "true\n";
-
-    cout << i << endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
